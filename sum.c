8cec6feeb2807ac61dde88e78546ca16b00571fa
robinm222 <74946508+robinm222@users.noreply.github.com>
2020-11-24 00:58:51 -0500

    Update and rename integer.c to sum.c

--- sum.c
+++ sum.c
@@ -2,15 +2,15 @@
 int main()
 {
     int n1;
-    int n2;
+    int n2,s;
 
     printf("Enter a number: ");
     scanf("%d", &n1);
     printf("Enter another number: ");
     scanf("%d", &n2);
-
-    printf("num1 = %d\n", n1);
-    printf("num2 = %d", n2);
+    s=n1-n2;
+    printf("sum = %d\n", s);
+    
 
     return 0;
 }
