d759a6198a49c3016ee33b0bd6bb3f13723234fe
robinm222 <74946508+robinm222@users.noreply.github.com>
2020-11-24 01:00:37 -0500

    Add files via upload

---
+++ different.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int n1;
+    int n2,s;
+
+    printf("Enter a number: ");
+    scanf("%d", &n1);
+    printf("Enter another number: ");
+    scanf("%d", &n2);
+    s=n1-n2;
+    printf("sum = %d\n", s);
+    
+
+    return 0;
+}
